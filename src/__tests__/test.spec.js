1e3a7836e96fc927682ad131862ab96d2794f01b
uryyyyyyy <koki305@gmail.com>
2016-12-14 04:59:31 +0900

    add test

---
+++ src/__tests__/test.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('test', () => {
+  it('1 + 2 = 3', () => {
+    const one:number = 1;
+    const two:number = 2;
+    expect(one + two).to.equal(3);
+  });
+});
